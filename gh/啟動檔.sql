5f521a9e7ad44ab1969b6a09c351af8d6023fa7c
ryanchen0204 <chenping9660@gmail.com>
2025-04-09 10:56:02 +0800

    Add files via upload

---
+++ gh/啟動檔.sql
@@ -0,0 +1,8 @@
+﻿create database msgboard;
+use msgboard;
+create table account (
+        idno int primary key auto_increment,
+		name varchar(64) not null
+		
+);
+insert into accoumt set name="user";
