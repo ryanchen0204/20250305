b416d045a35e53deb788f4547c8c9c3a7b6b1d57
ryanchen0204 <chenping9660@gmail.com>
2025-04-09 11:02:01 +0800

    Delete gh directory

--- gh/啟動檔.sql
+++
@@ -1,8 +0,0 @@
-﻿create database msgboard;
-use msgboard;
-create table account (
-        idno int primary key auto_increment,
-		name varchar(64) not null
-		
-);
-insert into accoumt set name="user";
